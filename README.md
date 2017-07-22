79b55b2b898c55d828dfe8298f20134d609dcfc6
Sarah Vessels <cheshire137@gmail.com>
2017-07-22 18:19:40 -0500

    Add command to open GitHub search

--- README.md
+++ README.md
@@ -8,14 +8,15 @@ A Chrome extension for navigating github.com via keyboard shortcuts.
 
 - **Option-Shift-R** - select GitHub repository
 - **Option-Shift-O** - select GitHub organization
+- **Option-Shift-S** - open global GitHub search
 
 ### Windows/Linux
 
 - **Alt-Shift-R** - select GitHub repository
 - **Alt-Shift-O** - select GitHub organization
+- **Alt-Shift-S** - open global GitHub search
 
 ## TODO
 
 - **Alt-Shift-F** - open file finder for selected repository
-- **Alt-Shift-S** - open global GitHub search
 - **Alt-Shift-T** - open teams for your selected organization
--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,11 @@
+function openGlobalSearch() {
+  const url = 'https://github.com/search/'
+  chrome.tabs.create({ url })
+}
+
 function openOptions(hash) {
   const url = `chrome-extension://${chrome.runtime.id}/options.html#${hash}`
-  chrome.tabs.create({ 'url': url })
+  chrome.tabs.create({ url })
 }
 
 chrome.commands.onCommand.addListener(function(command) {
@@ -8,6 +13,8 @@ chrome.commands.onCommand.addListener(function(command) {
     openOptions('select-repository')
   } else if (command === 'select-organization') {
     openOptions('select-organization')
+  } else if (command === 'global-search') {
+    openGlobalSearch()
   } else {
     console.debug('unhandled command:', command)
   }
