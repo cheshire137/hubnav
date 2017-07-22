4bed0157557f475428918370defc6e8a74212fb9
Sarah Vessels <cheshire137@gmail.com>
2017-07-22 18:16:06 -0500

    Show logo for repo's owner

--- extension/options.js
+++ extension/options.js
@@ -5,6 +5,7 @@ class OptionsPage {
 
   findElements() {
     this.repoInput = document.getElementById('repository')
+    this.repoLogo = document.getElementById('repo-logo')
     this.orgInput = document.getElementById('organization')
     this.orgLogo = document.getElementById('org-logo')
     this.optionsForm = document.getElementById('options-form')
@@ -31,7 +32,9 @@ class OptionsPage {
   hookUpHandlers() {
     this.optionsForm.addEventListener('submit', e => this.onSubmit(e))
     this.submitButton.addEventListener('click', e => e.currentTarget.blur())
+    this.repoInput.addEventListener('keyup', e => this.onRepoKeyup(e))
     this.orgInput.addEventListener('keyup', e => this.onOrgKeyup(e))
+    this.repoLogo.addEventListener('error', () => this.showDefaultRepoLogo())
     this.orgLogo.addEventListener('error', () => this.showDefaultOrgLogo())
   }
 
@@ -41,9 +44,23 @@ class OptionsPage {
     this.orgLogo.alt = org
   }
 
+  loadRepoLogo(rawRepo) {
+    let user = rawRepo.split('/')[0]
+    if (user && user.length > 0) {
+      user = encodeURIComponent(user)
+      this.repoLogo.src = `https://github.com/${user}.png`
+      this.repoLogo.alt = user
+    }
+  }
+
   showDefaultOrgLogo() {
     this.orgLogo.src = 'unknown-org.png'
     this.orgLogo.alt = ''
+  }
+
+  showDefaultRepoLogo() {
+    this.repoLogo.src = 'unknown-org.png'
+    this.repoLogo.alt = ''
   }
 
   onOrgKeyup(event) {
@@ -55,12 +72,30 @@ class OptionsPage {
     }, 750)
   }
 
+  onRepoKeyup(event) {
+    if (this.repoInputTimer) {
+      clearTimeout(this.repoInputTimer)
+    }
+    this.repoInputTimer = setTimeout(() => {
+      this.setRepoLogoSource()
+    }, 750)
+  }
+
   setOrgLogoSource() {
     const org = (this.orgInput.value || '').trim()
     if (org.length < 1) {
       this.showDefaultOrgLogo()
     } else {
       this.loadOrgLogo(org)
+    }
+  }
+
+  setRepoLogoSource() {
+    const repo = (this.repoInput.value || '').trim()
+    if (repo.length < 1) {
+      this.showDefaultRepoLogo()
+    } else {
+      this.loadRepoLogo(repo)
     }
   }
 
@@ -75,7 +110,10 @@ class OptionsPage {
 
   restoreOptions() {
     HubnavStorage.load().then(options => {
-      this.repoInput.value = options.repository || ''
+      if (options.repository && options.repository.length > 0) {
+        this.repoInput.value = options.repository
+        this.loadRepoLogo(options.repository)
+      }
       if (options.organization && options.organization.length > 0) {
         this.orgInput.value = options.organization
         this.loadOrgLogo(options.organization)
