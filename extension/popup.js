51a489869dcf7f5e10c38df765cd0a6aeda6b843
Sarah Vessels <cheshire137@gmail.com>
2017-07-22 19:48:56 -0500

    Highlight selected command briefly

--- extension/popup.js
+++ extension/popup.js
@@ -1,53 +1,3 @@
-function openOptions(hash) {
-  const url = `chrome-extension://${chrome.runtime.id}/options.html#${hash}`
-  chrome.tabs.create({ url })
-}
-
-function openFileFinder() {
-  HubnavStorage.load().then(options => {
-    if (options.repository && options.repository.length > 0) {
-      const parts = options.repository.split('/')
-      const owner = encodeURIComponent(parts[0])
-      const name = encodeURIComponent(parts[1])
-      const url = `https://github.com/${owner}/${name}/find/master`
-      chrome.tabs.create({ url })
-    } else {
-      openOptions('select-repository')
-    }
-  })
-}
-
-function openGlobalSearch() {
-  const url = 'https://github.com/search/'
-  chrome.tabs.create({ url })
-}
-
-function openTeams() {
-  HubnavStorage.load().then(options => {
-    if (options.organization && options.organization.length > 0) {
-      const org = encodeURIComponent(options.organization)
-      const url = `https://github.com/orgs/${org}/teams`
-      chrome.tabs.create({ url })
-    } else {
-      openOptions('select-organization')
-    }
-  })
-}
-
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'f') {
-    openFileFinder()
-  } else if (event.key === 't') {
-    openTeams()
-  } else if (event.key === 's') {
-    openGlobalSearch()
-  } else if (event.key === 'o') {
-    openOptions('select-organization')
-  } else if (event.key === 'r') {
-    openOptions('select-repository')
-  }
-})
-
 class PopupPage {
   constructor() {
     this.findElements()
@@ -56,9 +6,90 @@ class PopupPage {
   findElements() {
     this.repoReferences = document.querySelectorAll('.repository')
     this.orgReferences = document.querySelectorAll('.organization')
+    this.fShortcuts = document.querySelectorAll('.f-shortcut')
+    this.tShortcuts = document.querySelectorAll('.t-shortcut')
+    this.sShortcuts = document.querySelectorAll('.s-shortcut')
+    this.rShortcuts = document.querySelectorAll('.r-shortcut')
+    this.oShortcuts = document.querySelectorAll('.o-shortcut')
+  }
+
+  executeShortcut(action) {
+    if (this.shortcutTimer) {
+      clearTimeout(this.shortcutTimer)
+    }
+    this.shortcutTimer = setTimeout(action, 400)
+  }
+
+  openOptions(hash) {
+    const url = `chrome-extension://${chrome.runtime.id}/options.html#${hash}`
+    this.executeShortcut(() => chrome.tabs.create({ url }))
+  }
+
+  openFileFinder() {
+    HubnavStorage.load().then(options => {
+      if (options.repository && options.repository.length > 0) {
+        this.highlightShortcut(this.fShortcuts)
+        const parts = options.repository.split('/')
+        const owner = encodeURIComponent(parts[0])
+        const name = encodeURIComponent(parts[1])
+        const url = `https://github.com/${owner}/${name}/find/master`
+        this.executeShortcut(() => chrome.tabs.create({ url }))
+      } else {
+        this.openRepoSelect()
+      }
+    })
+  }
+
+  openRepoSelect() {
+    this.highlightShortcut(this.rShortcuts)
+    this.openOptions('select-repository')
+  }
+
+  openGlobalSearch() {
+    this.highlightShortcut(this.sShortcuts)
+    const url = 'https://github.com/search/'
+    this.executeShortcut(() => chrome.tabs.create({ url }))
+  }
+
+  openTeams() {
+    HubnavStorage.load().then(options => {
+      if (options.organization && options.organization.length > 0) {
+        this.highlightShortcut(this.tShortcuts)
+        const org = encodeURIComponent(options.organization)
+        const url = `https://github.com/orgs/${org}/teams`
+        this.executeShortcut(() => chrome.tabs.create({ url }))
+      } else {
+        this.openOrgSelect()
+      }
+    })
+  }
+
+  highlightShortcut(shortcuts) {
+    for (let i = 0; i < shortcuts.length; i++) {
+      shortcuts[i].classList.add('highlighted')
+    }
+  }
+
+  openOrgSelect() {
+    this.highlightShortcut(this.oShortcuts)
+    this.openOptions('select-organization')
   }
 
   setup() {
+    document.addEventListener('keydown', event => {
+      if (event.key === 'f') {
+        this.openFileFinder()
+      } else if (event.key === 't') {
+        this.openTeams()
+      } else if (event.key === 's') {
+        this.openGlobalSearch()
+      } else if (event.key === 'o') {
+        this.openOrgSelect()
+      } else if (event.key === 'r') {
+        this.openRepoSelect()
+      }
+    })
+
     HubnavStorage.load().then(options => {
       this.updateRepoReferences(options.repository)
       this.updateOrgReferences(options.organization)
