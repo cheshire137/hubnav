3a3ba5e80d0e4022d1d5781b043666d7dad936a8
Sarah Vessels <cheshire137@gmail.com>
2017-10-21 15:41:18 -0500

    As eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: 'airbnb-base',
+  globals: {
+    chrome: true,
+    document: true,
+    window: true,
+    SHORTCUTS: true,
+    HubnavStorage: true
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'padded-blocks': 0,
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    indent: ['error', 2, { SwitchCase: 1, CallExpression: { arguments: 'off' } }],
+    'function-paren-newline': ['error', 'consistent'],
+    'class-methods-use-this': 0,
+    'no-restricted-syntax': 0,
+    'guard-for-in': 0,
+    'dot-location': ['error', 'object']
+  }
+};
